4656dd051eba6beba20acef65845bb2b96c6a3ad
JAT <tjeanarnaud@gmail.com>
2021-04-14 01:32:53 -0500

    The Checkout Page

---
+++ src/components/Product.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../style/Product.css'
+
+const Product = ({ title, image, price, rating }) => {
+  return (
+    <div className="product">
+
+      <div className="product_info">
+
+        <p>{title}</p>
+
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+
+        <div className="product_rating">
+          {
+            Array(rating).fill().map((_, i) => (<p>⭐</p>))
+          }
+        </div>
+
+      </div>
+
+      <img src={image} alt="" />
+
+      <button className="product_to_cart">Add to Basket</button>
+
+    </div>
+  )
+}
+
+export default Product
